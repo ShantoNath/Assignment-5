8c3877a87430d5197cd675048f8c5c0000ed7ba5
ShantoNath <shantonath1998@gmail.com>
2024-02-19 23:07:20 +0600

    finished footer and modal section

--- JS/styles.js
+++ JS/styles.js
@@ -1,18 +1,18 @@
 const seats = document.getElementsByClassName('b-seat');
-// console.log(seats);
+
 
 let seatCount = 1;
 let ticketPrice = 550;
 let totalSeat = 39;
+let selectedSeat = 0;
 for(const seat of seats){
     // console.log(seat);
 
-    seat.addEventListener('click', function(){
+    seat.addEventListener('click', function(e){
         const seatNumber = seat.innerText;
-        // console.log(seatNumber);
+        
 
         const ticketDetails = document.getElementById('ticket-details');
-        // console.log(ticketDetails);
 
         const economy = 'Economy';
         const p2 = document.createElement('p');
@@ -30,10 +30,8 @@ for(const seat of seats){
         ticketDetails.appendChild(li);
 
         // price count 
-        
-        const totalPrice = parseFloat(document.getElementById('total-price').innerText);
-        const tPrice = parseFloat(document.getElementById('t-price').innerText);
-        document.getElementById('total-price').innerText = totalPrice + tPrice;
+        const totalPrice = parseFloat(document.getElementById('total-price').innerText = seatCount * 550);
+        seat.disabled = true;
 
         // confirm  Seat 
         const confirmSeat = parseInt(document.getElementById('confirm-Seat').innerText);
@@ -43,7 +41,59 @@ for(const seat of seats){
         // total seat 
         const totalSit = document.getElementById('total-sit').innerText;
         document.getElementById('total-sit').innerText = totalSeat--;
-        // console.log(totalSeat--);
         
-    })
+
+        event.target.style.backgroundColor = '#1DD100';
+        event.target.style.color = '#f8fafc';
+        
+        if( seatCount  > 5){
+            alert('Sorry you can not buy Anymore');
+            seatNumber.disabled = true;
+            return
+        }
+        
+        
+
+     //----------------- coupon input field -------------------------
+        const btn = document.getElementById('apply-btn');
+        btn.addEventListener('click', function(){
+        
+
+            // get the value  from input 
+            const couponElement = document.getElementById('input-field').value;
+           
+            if(totalPrice >= 2200){
+                if(couponElement === 'NEW15'){
+                    const discountPrice = document.getElementById('discount-price');
+                    const discountAmount = totalPrice * 0.15; 
+                    discountPrice.innerText = discountAmount.toFixed(2);
+                    // grand total 
+                    const grandTotal = document.getElementById('grand-total');
+                    grandTotal.innerText = totalPrice - discountAmount.toFixed(2);              
+                    document.getElementById('input-field').classList.add('hidden');
+                    btn.classList.add('hidden');
+                }
+                else if(couponElement === 'Couple 20'){
+                    const discountPrice = document.getElementById('discount-price');
+                    const discountAmount = totalPrice * 0.2; 
+                    discountPrice.innerText = discountAmount.toFixed(2);
+                    // grand total 
+                    const grandTotal = document.getElementById('grand-total');
+                    grandTotal.innerText = totalPrice - discountAmount.toFixed(2);              
+                    document.getElementById('input-field').classList.add('hidden');
+                    btn.classList.add('hidden');
+                }
+                
+                else{
+                    alert('Invalid Coupon')
+                }
+            }
+            else{
+                // alert('please buy ticket more');
+            }
+
+        
+        })
+        
+    }) 
 }
